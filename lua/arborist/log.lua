0bfa86584d3df84073f6b445516892f20bb17c70
Stephen Waits <steve@waits.net>
2026-04-11 19:00:31 -0600

    refactor: shrink log.lua with loop-generated methods

--- lua/arborist/log.lua
+++ lua/arborist/log.lua
@@ -1,19 +1,10 @@
 --- Schedule-safe notifications. All output goes through vim.notify on the main thread.
 local M = {}
 
---- @param msg string
-function M.info(msg)
-  vim.schedule(function() vim.notify(msg, vim.log.levels.INFO, { title = "arborist" }) end)
-end
-
---- @param msg string
-function M.warn(msg)
-  vim.schedule(function() vim.notify(msg, vim.log.levels.WARN, { title = "arborist" }) end)
-end
-
---- @param msg string
-function M.error(msg)
-  vim.schedule(function() vim.notify(msg, vim.log.levels.ERROR, { title = "arborist" }) end)
+for _, level in ipairs({ "info", "warn", "error" }) do
+  M[level] = function(msg)
+    vim.schedule(function() vim.notify(msg, vim.log.levels[level:upper()], { title = "arborist" }) end)
+  end
 end
 
 return M
